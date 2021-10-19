2017af169da940c7b1f060bcd0511c84f29998c9
Catsil <imgloriasilva@gmail.com>
2021-10-19 18:52:46 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const PRONUONS = ["the", "our"];
+const ADJS = ["great", "big"];
+const NOUNS = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const EXTS = ["er", "es", "oon"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domains = dom();
+  document.querySelector("#domain").innerHTML = domains;
 };
+function dom() {
+  let alldomains = [];
+  for (let i = 0; i < PRONUONS.length; i++) {
+    for (let j = 0; j < ADJS.length; j++) {
+      for (let k = 0; k < NOUNS.length; k++) {
+        for (let l = 0; l < EXTS.length; l++) {
+          let word = PRONUONS[i] + ADJS[j] + NOUNS[k];
+          let lengthExtend = EXTS[l].length;
+          let last = word.slice(-lengthExtend);
+          if (last === EXTS[l]) {
+            let newWord = word.replace(last, "." + last);
+            alldomains.push(newWord);
+          } else {
+            alldomains.push(word + "." + EXTS[l]);
+          }
+        }
+      }
+    }
+  }
+  return alldomains;
+}
